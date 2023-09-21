f0ddd1cf704ab5c9c41fcaa265a838dc8690ff6a
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-09-22 01:39:20 +0700

    update: add enforcedDonator & MedicalStaff & Moderator procedures

--- apps/client/src/lib/server/procedure.ts
+++ apps/client/src/lib/server/procedure.ts
@@ -18,4 +18,37 @@ const enforcedAuth = t.middleware(({ ctx, next }) => {
 	});
 });
 
+const enforcedDonator = t.middleware(({ ctx, next }) => {
+	if (!ctx.session?.donator_id) {
+		throw new TRPCError({ code: 'UNAUTHORIZED' });
+	}
+
+	return next({
+		ctx
+	});
+});
+
+const enforcedMedicalStaff = t.middleware(({ ctx, next }) => {
+	if (!ctx.session?.medical_staff_id) {
+		throw new TRPCError({ code: 'UNAUTHORIZED' });
+	}
+
+	return next({
+		ctx
+	});
+});
+
+const enforcedModerator = t.middleware(({ ctx, next }) => {
+	if (!ctx.session?.moderator_id) {
+		throw new TRPCError({ code: 'UNAUTHORIZED' });
+	}
+
+	return next({
+		ctx
+	});
+});
+
 export const protectedProcedure = t.procedure.use(enforcedAuth);
+export const donatorProcedure = t.procedure.use(enforcedDonator);
+export const medicalStaffProcedure = t.procedure.use(enforcedMedicalStaff);
+export const moderatorProcedure = t.procedure.use(enforcedModerator);
