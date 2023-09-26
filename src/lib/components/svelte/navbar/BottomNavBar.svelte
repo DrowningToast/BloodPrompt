5a2b967d82b306b1ba87dace932ed6628c7b7463
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-09-26 18:41:48 +0700

    add: bottom-navigation

---
+++ src/lib/components/svelte/navbar/BottomNavBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { CalendarClock, FileClock, Gift, Home } from 'lucide-svelte';
+</script>
+
+<div class="rounded-3xl bg-[#F5222D] fixed bottom-4 w-full max-w-[90%]">
+	<div class="py-5 flex flex-row gap-4 items-center justify-between px-10 text-white">
+		<button>
+			<Home size={28} />
+		</button>
+		<button>
+			<CalendarClock size={28} />
+		</button>
+		<button>
+			<FileClock size={28} />
+		</button>
+		<button>
+			<Gift size={28} />
+		</button>
+	</div>
+</div>
