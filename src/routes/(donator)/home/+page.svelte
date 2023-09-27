355bc382fb14b70002b615873a0c367677acb293
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-09-27 21:15:19 +0700

    fix: /home conflict

--- src/routes/(donator)/home/+page.svelte
+++ src/routes/(donator)/home/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Banner from './Banner.svelte';
+	import SpecialEvent from './SpecialEventHero.svelte';
+
+	import Searchbar from './Searchbar.svelte';
+	import Announcement from './Announcement.svelte';
+	import AnnouncementList from './AnnouncementList.svelte';
 
 	export let data: PageData;
 </script>
 
-<div class="m-12">
-	<p>Welcome</p>
-	<a href="/reservation">จองคิว</a>
-</div>
+<Banner />
+<section class="px-10 py-8">
+	<SpecialEvent />
+</section>
+<section class="px-8">
+	<Searchbar />
+</section>
+<section>
+	<AnnouncementList />
+</section>
