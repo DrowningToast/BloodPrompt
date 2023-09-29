277ad04e0837db16d4ad3f80e0852a25d48c26f1
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-09-30 01:12:21 +0700

    add: login protected route (redirect when not logged in)

---
+++ src/routes/moderator/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { trpc } from '$lib/trpc';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	trpc.auth.getUser
+		.query()
+		.then((res) => {
+			if (!res) {
+				alert('โปรดเข้าสู่ระบบเพื่อดำเนินการต่อ...');
+				goto('/staff/login');
+			}
+		})
+		.catch((error) => console.error(error));
+</script>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>
